5db62a5079cf4a57e67c389e8d75f6b002d1649a
Giuseppe Bellomo <g.bellomo@reply.it>
2019-06-01 18:27:40 +0200

    created layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatFormFieldModule, MatCardModule, MatButtonModule, MatIconModule, MatNavList, MatListModule, MatListItem } from '@angular/material';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { CommitsPageComponent } from './pages/commits-page/commits-page.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommitsListComponent } from './components/commits-list/commits-list.component';
+import { CommitDetailsComponent } from './components/commit-details/commit-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent,
+    CommitsPageComponent,
+    CommitsListComponent,
+    CommitDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
