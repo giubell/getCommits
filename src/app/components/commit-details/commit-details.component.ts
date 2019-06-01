071bc67dc7643e9634594968ca13c4c17f9529fc
Giuseppe Bellomo <g.bellomo@reply.it>
2019-06-01 18:30:05 +0200

    created commit details

---
+++ src/app/components/commit-details/commit-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-commit-details',
+  templateUrl: './commit-details.component.html',
+  styleUrls: ['./commit-details.component.scss']
+})
+export class CommitDetailsComponent implements OnInit {
+
+  @Input() public commit:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
